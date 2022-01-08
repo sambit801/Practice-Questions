71885f02eeabbd894385de2c6df99f3fb9d7c671
Sambit Kumar Nayak <77061340+sambit801@users.noreply.github.com>
2022-01-08 19:30:38 +0530

    Update index.js

--- Array_Concat/index.js
+++ Array_Concat/index.js
@@ -1,1 +1,15 @@
+function concatArray(){
 
+const arr1=['Sambit','1','2','3','true'];
+
+const arr2=['6','7','8'];
+
+const arr3=['false','4','5'];
+
+const SingleArr = arr1.concat(arr2,arr3);
+
+console.log(SingleArr);
+
+}
+
+concatArray()
