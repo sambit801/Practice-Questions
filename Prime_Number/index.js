5958584216742e73d35dd61084781ea10fa9c4a0
Sambit Kumar Nayak <77061340+sambit801@users.noreply.github.com>
2022-01-08 19:17:05 +0530

    Update index.js

--- Prime_Number/index.js
+++ Prime_Number/index.js
@@ -1,1 +1,20 @@
 
+function checkPrime(){
+let count=0;
+let i,j;
+for(j=1;j<=100;j++)
+{
+for( i=1;i<=j;i++)
+{
+  if(j%i==0)
+  count++;
+}
+
+if(count==2)
+console.log(j);
+count=0;
+
+}
+  
+}
+checkPrime();
