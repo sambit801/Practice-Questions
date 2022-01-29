fe770195ad44e11c1372b42f47c853e4ef79b554
Sambit Kumar Nayak <77061340+sambit801@users.noreply.github.com>
2022-01-29 14:02:29 +0530

    Update index.js

--- MultipleArray_Merge&Sort/index.js
+++ MultipleArray_Merge&Sort/index.js
@@ -1,17 +1,17 @@
+
 function mergeArrays(...arrays) { 
 
-    
         let mergedArray = []; 
-
-        arrays.forEach(array =>{
-
-            mergedArray = mergedArray.concat(array).sort();
-
+// Using forEach() 
+        arrays.forEach(array=>{
+// Concat all the arrays and then sort by ascending order
+       mergedArray = mergedArray.concat(array).sort();
         });
-
- 
-        console.log(mergedArray); 
-
+        
+        let result = mergedArray.filter(function(item,pos){
+          return mergedArray.indexOf(item) == pos;
+        });
+        console.log(result); 
     }
     
    mergeArrays([1,2,4,5], [2,4,6,7,8],[5,8,6,5],[3,5,7,8]);
